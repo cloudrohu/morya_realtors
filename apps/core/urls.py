5720a6b9b0e760a28abd367e11f3ae3a1da20469
Renuka Tiwari <cloudrohu@gmail.com>
2026-09-07 13:56:03 +0530

    Attention

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # Accessing http://127.0.0 triggers the view
     path('about/', views.about, name='about'),
     path('faqs/', views.FAQs, name='faqs'),
+    path('developers/', views.developers, name='developers'),
+    path('localities/', views.localities, name='localities'),
 
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -6,6 +6,15 @@ def about(request):
   
     return render(request, 'home/about.html',)
 
+
+def developers(request):
+  
+    return render(request, 'home/developers.html',)    
+
+def localities(request):
+  
+    return render(request, 'home/localities.html',)    
+
 def FAQs(request):
   
     return render(request, 'home/faqs.html',)
