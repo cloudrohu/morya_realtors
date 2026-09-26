d9ca33011b39937b4d6bfc8e244093ce2dde0884
Renuka Tiwari <cloudrohu@gmail.com>
2026-09-26 13:22:55 +0530

    Yad (Russian)

--- apps/properties/models.py
+++ apps/properties/models.py
@@ -1057,7 +1057,7 @@ class Connectivity(BaseModel):
     
 class Amenities(BaseModel):
     Project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="amenities")
-    amenities = models.ForeignKey(Amenity, on_delete=models.CASCADE, related_name="amenities")
+    amenities = models.ForeignKey(ProjectAmenities, on_delete=models.CASCADE, related_name="amenities")
     
     def __str__(self):
         return f"{self.Project.project_name} - {self.amenities.title}"
